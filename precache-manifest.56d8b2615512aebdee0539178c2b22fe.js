c0974da48c1e54b62889179412f676964141051e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-10 06:03:22 +0000

    Deploy luca-borrione/reactnd-project2-wouldyourather to github.com/luca-borrione/reactnd-project2-wouldyourather.git:gh-pages

---
+++ precache-manifest.56d8b2615512aebdee0539178c2b22fe.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/reactnd-project2-wouldyourather/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "bd9f4e6fe2bb697d50d3",
+    "url": "/reactnd-project2-wouldyourather/static/css/main.acb6453b.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/reactnd-project2-wouldyourather/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "925c1ac31a48c90f4cf2",
+    "url": "/reactnd-project2-wouldyourather/static/js/1.925c1ac3.chunk.js"
+  },
+  {
+    "revision": "d127b49e2319b108a8ea",
+    "url": "/reactnd-project2-wouldyourather/static/js/runtime~main.d127b49e.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/reactnd-project2-wouldyourather/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/reactnd-project2-wouldyourather/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/reactnd-project2-wouldyourather/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/reactnd-project2-wouldyourather/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/reactnd-project2-wouldyourather/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/reactnd-project2-wouldyourather/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "bd9f4e6fe2bb697d50d3",
+    "url": "/reactnd-project2-wouldyourather/static/js/main.bd9f4e6f.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/reactnd-project2-wouldyourather/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/reactnd-project2-wouldyourather/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/reactnd-project2-wouldyourather/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/reactnd-project2-wouldyourather/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/reactnd-project2-wouldyourather/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/reactnd-project2-wouldyourather/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/reactnd-project2-wouldyourather/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/reactnd-project2-wouldyourather/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "925c1ac31a48c90f4cf2",
+    "url": "/reactnd-project2-wouldyourather/static/css/1.1846f1a3.chunk.css"
+  },
+  {
+    "revision": "28822fdb2e20edd01504829dc67a0e7c",
+    "url": "/reactnd-project2-wouldyourather/index.html"
+  }
+];
